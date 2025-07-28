ea50426b459fa3217caddcb63c1c5bf2a2cae564
mohamed1312131 <mohamed1312131@gmail.com>
2025-07-28 13:38:27 +0100

    fix image

--- src/app/homePageSubComponent/thank-you/thank-you.component.ts
+++ src/app/homePageSubComponent/thank-you/thank-you.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FrontService, Carousel } from '../../services/front.service';
 
 @Component({
   selector: 'app-thank-you',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './thank-you.component.html',
   styleUrl: './thank-you.component.css'
 })
@@ -18,6 +20,7 @@ export class ThankYouComponent implements OnInit {
       if (data && data.length > 0) {
         // Adjust property name if your Carousel type uses a different field
         this.firstImageUrl = data[0].imageUrl;
+        console.log('First carousel image URL:', this.firstImageUrl);
       }
     });
   }
