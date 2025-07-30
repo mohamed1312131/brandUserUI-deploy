551c308630a5fbac4f22f5617b65f6442335808a
mohamed1312131 <mohamed1312131@gmail.com>
2025-07-30 22:20:45 +0100

    make faq dynamic

--- src/app/homePageSubComponent/faq/faq.component.ts
+++ src/app/homePageSubComponent/faq/faq.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Inject,
+  PLATFORM_ID
+} from '@angular/core';
+import {
+  CommonModule,
+  isPlatformBrowser
+} from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrls: ['./faq.component.css']
 })
-export class FAQComponent {
+export class FAQComponent implements OnInit {
+  isBrowser: boolean;
+  faqs: any[] = [];
 
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.http.get<any[]>(`${environment.apiUrl}/faq`).subscribe({
+        next: (data) => this.faqs = data,
+        error: (err) => console.error('Failed to load FAQs', err)
+      });
+    }
+  }
 }
